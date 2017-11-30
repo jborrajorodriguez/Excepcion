b6c9c02cf190db55686b0d00d00d60a67b37f967
jborrajorodriguez <jborrajorodriguez@victoria22.danielcastelao.org>
2017-11-30 12:48:52 +0100

    Clase variosCatch

---
+++ src/excepcion/VariosCatch.java
@@ -0,0 +1,36 @@
+package excepcion;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author Juan Borrajo Rodriguez Nº5937
+ */
+public class VariosCatch {
+    private int numerador, denominador;
+
+    /**
+     * Constructor por defecto.
+     */
+    public VariosCatch() {
+    }
+    /**
+     * Metodo que no devuelve nada dividirCapturaVariosCatch.
+     */
+    public void dividirCapturaVariosCatch() {
+        try{
+        numerador=Integer.parseInt(JOptionPane.showInputDialog("Teclea numerador"));//Se pide el numerador por joption pane y se pasa de string a un int mediante integer.parseint.
+        denominador=Integer.parseInt(JOptionPane.showInputDialog("Teclea el denominador"));//Se pide el denominador por joption pane y se pasa de string a un int mediante integer.parseint.
+        
+            int resultado=numerador/denominador;//Resultado de la division del numerador entre el denominador.
+            JOptionPane.showMessageDialog(null,"Resultado = "+resultado);
+        } catch (ArithmeticException e) {
+            JOptionPane.showMessageDialog(null,"Error");
+            //e.printStackTrace();
+        } catch(Exception e2){
+            JOptionPane.showMessageDialog(null,"Introduce solo números");
+        }
+
+    }
+
+
+}
